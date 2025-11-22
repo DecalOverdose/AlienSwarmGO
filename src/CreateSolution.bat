492828b22d9efea1bf6c7cea104119d7989a1c2d
DecalOverdose <mohamedashraf1829982@gmail.com>
2025-11-22 03:31:54 -0800

    Initial MSVC 143 support

--- src/CreateSolution.bat
+++ src/CreateSolution.bat
@@ -1,2 +1,2 @@
-devtools\bin\vpc.exe /hl2_template +game /mksln Game_HL2_Template-2019.sln /2013 /define:VS2019
+devtools\bin\vpc.exe /hl2_template +game /mksln Game_HL2_Template-2022.sln /2013 /define:VS2022
 pause
